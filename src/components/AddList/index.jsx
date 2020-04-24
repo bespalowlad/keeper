040a91c29743e158192203d33c77f34553550528
Vladyslav <bespalov.v@brights.cloud>
2020-04-24 17:41:42 +0300

    Add and remove menu item is done

--- src/components/AddList/index.jsx
+++ src/components/AddList/index.jsx
@@ -5,9 +5,31 @@ import Badge from '../Badge';
 import './AddList.scss'
 import closeSvg from '../../assets/images/close.svg';
 
-const AddButtonList = ({ colors }) => {
+const AddButtonList = ({ colors, onAddList }) => {
     const [visiblePopup, setVisiblePopup] = useState(false);
     const [selectedColor, setSelectedColor] = useState(colors[0].id);
+    const [inputValue, setInputValue] = useState('');
+
+    const onClose = () => {
+        setVisiblePopup(false);
+        setSelectedColor(colors[0].id);
+        setInputValue('');
+    }
+
+    const AddList = () => {
+        if (!inputValue) {
+            alert('Введите название папки')
+            return;
+        }
+
+        onAddList({
+            id: Math.random(),
+            name: inputValue,
+            colorId: selectedColor,
+            color: colors.find(c => c.id === selectedColor).name
+        });
+        onclose();
+    }
 
     return <div className="add-list">
         <List
@@ -21,16 +43,23 @@ const AddButtonList = ({ colors }) => {
                             <path d="M1 6H11" stroke="#868686" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
                         </svg>
                     ),
-                    text: 'Добавить папку'
+                    name: 'Добавить папку'
                 }
             ]}
         />
+
         {visiblePopup && <div className="add-list-popup">
             <div className="close">
-                <img onClick={() => setVisiblePopup(false)} src={closeSvg} className="close" alt="Close" />
+                <img onClick={onClose} src={closeSvg} className="close" alt="Close" />
             </div>
 
-            <input className="field" type="text" placeholder="Название папки" />
+            <input
+                value={inputValue}
+                onChange={e => setInputValue(e.target.value)}
+                className="field"
+                type="text"
+                placeholder="Название папки"
+            />
 
             <div className="color-list">
                 {colors.map(color => (
@@ -43,7 +72,7 @@ const AddButtonList = ({ colors }) => {
                 ))}
             </div>
 
-            <button className="button">Добавить</button>
+            <button onClick={AddList} className="button">Добавить</button>
         </div>}
     </div>
 
--- src/components/List/index.jsx
+++ src/components/List/index.jsx
@@ -4,18 +4,28 @@ import Badge from '../Badge';
 
 import './List.scss'
 
-const List = ({ items, isRemovable, onClick }) => (
-    <ul onClick={onClick} className="list">
+import removeSVG from '../../assets/images/remove.svg';
+
+const List = ({ items, isRemovable, onClick, onRemove }) => {
+
+    const beforeRemove = ({ id, name }) => {
+        if (window.confirm(`Вы действительно хотите удалить ${name}?`)) {
+            onRemove(id)
+        }
+    }
+
+    return <ul onClick={onClick} className="list">
         {items.map((item, index) => (
             <li className={classNames(item.className, { 'active': item.active })} key={index}>
                 {item.icon ?
                     item.icon :
                     <Badge color={item.color} />
                 }
-                <p>{item.text}</p>
+                <p>{item.name}</p>
+                {isRemovable && <img src={removeSVG} onClick={() => beforeRemove(item)} className="remove-btn" alt="Remove" />}
             </li>
         ))}
     </ul>
-)
+}
 
 export default List
