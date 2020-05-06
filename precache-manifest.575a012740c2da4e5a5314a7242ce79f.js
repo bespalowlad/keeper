105b4838fe38f3757ab4cddc07ab602559cb4fb2
Vladyslav <bespalov.v@brights.cloud>
2020-05-06 17:39:41 +0300

    Updates

---
+++ precache-manifest.575a012740c2da4e5a5314a7242ce79f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79f57519f254ebb1e3d68500196f3586",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "f7f71d7f407e6f1eaee0",
+    "url": "/keeper/static/css/main.4ae577bd.chunk.css"
+  },
+  {
+    "revision": "d580cfc858ce6402202d",
+    "url": "/keeper/static/js/2.e51008e6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/keeper/static/js/2.e51008e6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7f71d7f407e6f1eaee0",
+    "url": "/keeper/static/js/main.f7f0a866.chunk.js"
+  },
+  {
+    "revision": "6d7f21cc2dd2a068fce8",
+    "url": "/keeper/static/js/runtime-main.c0064e59.js"
+  },
+  {
+    "revision": "14e2f379ea2f16993535086cc7b87b36",
+    "url": "/keeper/static/media/close.14e2f379.svg"
+  },
+  {
+    "revision": "93ca2630f322fbd7b9e12cf9918311d1",
+    "url": "/keeper/static/media/edit.93ca2630.svg"
+  },
+  {
+    "revision": "ba9a0c8a51696323be441671bb7439c5",
+    "url": "/keeper/static/media/plus.ba9a0c8a.svg"
+  },
+  {
+    "revision": "71708d87e237bd0eb45365e4dc6fb7d9",
+    "url": "/keeper/static/media/remove.71708d87.svg"
+  }
+]);
